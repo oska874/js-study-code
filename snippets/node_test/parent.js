0dd28d5f668e6e82e35f5e7118e36a8632b8a081
ezio <ezio_zhang@outlook.com>
2015-10-14 11:04:45 +0800

    node test
    1. child_process should modify std-output or will not see its output
    2. spawn(<env>,<cmd>,<stdio cfg>

--- snippets/node_test/parent.js
+++ snippets/node_test/parent.js
@@ -1,7 +1,7 @@
 var child_process = require('child_process');
 
 /* parent.js */
-var child = child_process.spawn('node', [ 'child.js' ]);
+var child = child_process.spawn('node', [ 'child.js' ],{stdio:[0,1,2]});
 console.log("abcabcabcabc");
 
 console.log("000");
@@ -13,5 +13,5 @@ while( t1++ < 100)
     console.log("010");
 }
 */
-child.kill('SIGTERM');
+setTimeout(function(){child.kill('SIGTERM');},2000);
 console.log("zzz");
